b9c5c6b238941aed3220f6ab9beb57d563264022
Jason Barnett <jayb21@neo.rr.com>
2019-03-20 21:33:02 -0400

    Added a dynamic port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-const PORT = 1000;
+const PORT = process.env.PORT || 1000;
 
 app.use(express.static(__dirname));
 
