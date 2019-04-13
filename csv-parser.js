ba6e542da5a0849dcbfe157a8d0d9ca2e0b81dfe
Jason Barnett <jayb21@neo.rr.com>
2019-04-13 14:32:35 -0400

    Trying to get the data formatted correctly

--- csv-parser.js
+++ csv-parser.js
@@ -15,6 +15,8 @@ const csvFeatures = [
   ];
 
 const csvLabel = 'state';
+const fieldsToKeep = csvFeatures.concat(csvLabel);
+
 const allowedLabelValues = [
     'successful',
     'failed',
@@ -27,90 +29,110 @@ const modFns = {
     }
 }
 
-const filterLabelField = (dataset) => {
-    // let labelIdx = null;
-    return dataset.filter((row) => {
-        // // ignore header row
-        // if (!idx) {
-        //     labelIdx = row.findIndex(f => f === csvLabel);
-        //     return true;
-        // }
+const fieldsToConvertToNumbers = [
+    'category',
+    'country',
+    'spotlight',
+    'staff_pick',
+    'state'
+]
+const fieldNumToValMap = {};
 
-        return allowedLabelValues.includes(row[csvLabel]);
+const mapFieldValues = dataset => {
+    const mapper = dataset.reduce((acc, cur) => {
+        fieldsToConvertToNumbers.forEach(field => {
+            if (!acc[field]) {
+                acc[field] = {};
+            }
+
+            // console.log(acc); 
+            
+            if (acc[field][cur[field]] === undefined) {
+                const newId = Object.keys(acc[field]).length
+                acc[field][cur[field]] = newId;
+            }
+            // console.log(acc); 
+        })
+        return acc;
+    }, {});
+    fs.writeFile('./csv_value_mapper.json', JSON.stringify(mapper, null, 4), 'utf8', err => {
+        if (err) {
+            console.log(err);
+        }
+    })
+    return mapper;
+}
+
+const translateFieldValues = dataset => {
+    const mapper = mapFieldValues(dataset);
+
+    dataset = dataset.map(row => {
+        fieldsToConvertToNumbers.forEach(field => {
+            row[field] = mapper[field][row[field]];
+        })
+
+        Object.keys(row).forEach(f => {
+            row[f] = Number(row[f]);
+        })
+        
+        return row;
+    })
+
+    // send to CSV file
+    converter.json2csv(dataset, (err, data) => {
+        if (err) {
+            return console.log(err);
+        }
+        fs.writeFile('./data_sets/csv_parser_output.csv', data, 'utf8', err => {
+            if (err) {
+                console.log(err);
+            }
+        })
+    })
+
+    // send to JSON file
+    const features = dataset.map(row => {
+        let rowArr = [];
+        csvFeatures.forEach(field => {
+            rowArr = rowArr.concat(row[field])
+        })
+        return rowArr;
+    })
+    const labels = dataset.map(row => {
+        let rowArr = [];
+        rowArr = rowArr.concat(row[csvLabel]);
+        return rowArr;
+    })
+    const allData = {
+        features,
+        labels
+    }
+    fs.writeFile('./test.json', JSON.stringify(allData, null, 4), 'utf8', err => {
+        if (err) {
+            console.log(err)
+        }
     })
 }
 
-const getCategorySummary = dataset => {
-    let summary = dataset.reduce((acc, cur) => {
-        const catName = cur.category;
-        
-        if (!acc[catName]) {
-            acc[catName] = 1
-        } else {
-            acc[catName] = acc[catName] + 1;
-        }
-        return acc;
-    }, {})
-
-    /**
-     * trying to sort the category names... Not working so far
-     */
-    // summary = Object.entries(summary)
-    //     .sort((a, b) => b[0][0] < a[0][0])
-    //     .reduce((acc, cur) => {
-    //         const catName = cur[0];
-
-    //         if (!acc[catName]) {
-    //             acc[catName] = 1
-    //         } else {
-    //             acc[catName] = acc[catName] + 1;
-    //         }
-    //         return acc;
-    //     }, {})
+const filterLabelField = (dataset) => {
+    return dataset.filter((row) => allowedLabelValues.includes(row[csvLabel]));
 }
+
+// const getCategorySummary = dataset => {
+//     let summary = dataset.reduce((acc, cur) => {
+//         const catName = cur.category;
+        
+//         if (!acc[catName]) {
+//             acc[catName] = 1
+//         } else {
+//             acc[catName] = acc[catName] + 1;
+//         }
+//         return acc;
+//     }, {})
+// }
 
 const cleanData = dataset => {
     // const fields = dataset[0];
-    const fieldsToKeep = csvFeatures.concat(csvLabel);
-    // const fieldsToKeepIndexes = fieldsToKeep.reduce((acc, cur) => {
-    //     const idx = fields.findIndex(f => f === cur);
-    //     if (idx !== -1) {
-    //         acc[cur] = idx;
-    //         acc[idx] = cur;
-    //     }
-    //     return acc;
-    // }, {});
-
-    // console.log(fieldsToKeepIndexes)
-    // filter the dataset
-    // dataset = dataset.map((row, rowIdx) => {
-    //     // map the row and modify any needed fields
-    //     let newRow = row.map((fieldContent, idx) => {
-    //         // skip any mutation for header row
-    //         if (!rowIdx) {
-    //             return fieldContent;
-    //         }
-
-    //         // map over each field and see if it needs modified
-    //         const field = fieldsToKeepIndexes[idx]
-    //         if (modFns[field]) {
-    //             return modFns[field](fieldContent, rowIdx);
-    //         }
-    //         return fieldContent;
-    //     })
-
-    //     // filter out only the columns that are wanted
-    //     newRow = newRow.filter((fieldContent, idx) => {
-    //         const field  = fieldsToKeepIndexes[idx];
-    //         if ( field ) {
-    //             return true;
-    //         }
-    //         return false;
-    //     })
-
-    //     return newRow;
-    // })
-
     dataset = dataset.map(row => {
         const filteredRow = {};
 
@@ -128,37 +150,8 @@ const cleanData = dataset => {
     // console.log(fieldsToKeepIndexes);
     dataset = filterLabelField(dataset)
     
-    converter.json2csv(dataset, (err, data) => {
-        if (err) {
-            return console.log(err);
-        }
-        fs.writeFile('./data_sets/csv_parser_output.csv', data, 'utf8', err => {
-            if (err) {
-                console.log(err);
-            }
-        })
-    })
-    
+    translateFieldValues(dataset);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
---
+++ train.js
@@ -0,0 +1,84 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const tf = require('@tensorflow/tfjs-node');
+
+const PORT = process.env.PORT || 1000;
+
+app.use(express.static(__dirname));
+
+app.use(cors());
+
+csvLabel = 'state';
+const columnConfigs = {};
+columnConfigs[csvLabel] = { 
+    isLabel: true,
+    // hasHeader: true
+ };
+
+const csvUrl =
+'http://localhost:1000/data_sets/csv_parser_output.csv';
+  // 'https://tensorflow-kickstarter.herokuapp.com/data_sets/K001.csv'
+//   'http://localhost:1000/data_sets/K001.csv';
+
+
+async function run() {
+    // We want to predict the column "medv", which represents a median value of
+    // a home (in $1000s), so we mark it as a label.
+    const csvDataset = tf.data.csv(
+      csvUrl, {
+        columnConfigs
+      });
+ 
+    // Number of features is the number of column names minus one for the label
+    // column.
+    const numOfFeatures = (await csvDataset.columnNames()).length - 1;
+    console.log(`# of features: ${numOfFeatures}`)
+ 
+    // Prepare the Dataset for training.
+    const flattenedDataset =
+      csvDataset
+      .map(({xs, ys}) =>
+        {
+          // Convert rows from object form (keyed by column name) to array
+          // form.
+          return {xs:Object.values(xs), ys:Object.values(ys)};
+        })
+      .batch(10);
+ 
+    // Define the model.
+    const model = tf.sequential();
+    model.add(tf.layers.dense({
+      inputShape: [numOfFeatures],
+      units: 1
+    }));
+    model.compile({
+      optimizer: tf.train.sgd(0.000001),
+      loss: 'meanSquaredError'
+    });
+ 
+    // Fit the model using the prepared Dataset
+    await model.fitDataset(flattenedDataset, {
+      epochs: 10,
+      callbacks: {
+        onEpochEnd: async (epoch, logs) => {
+          console.log(epoch + ':' + logs.loss);
+        },
+        onTrainEnd: async(logs) => {
+  
+        }
+      }
+    });
+
+    try {
+        model.predict(tf.tensor([[30],[0],[0],[1000],[1570.753],[0],[0]])).print();
+    } catch (error) {
+        console.log(error)
+    }
+ }
+
+ app.listen(PORT, () => {
+    console.log('Kickstarter listening on port 1000');
+})
+ 
+ run();
