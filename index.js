cc9b08487b56b2593d8d9653cfa73c650400af49
Jason Barnett <jayb21@neo.rr.com>
2019-04-16 01:06:23 -0400

    Created a 2d model, saved it, and loaded it

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-// Define a model for linear regression.
-const model = tf.sequential({
-    layers: [
-        tf.layers.dense({ inputShape: [1], units: 1 })
-    ]
-});
-model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
+const tf = require('@tensorflow/tfjs-node');
 
-const arrTests = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const arrLabels = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+// Define a model for linear regression.
+// const model = tf.sequential({
+//     layers: [
+//         tf.layers.dense({ inputShape: 2, units: 1, activation: 'sigmoid' }),
+//         tf.layers.dense({ units: 1 })
+//     ]
+// });
+// model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
+
+// const arrTests = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const arrLabels = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
+
+const arrTests = [[1, 1], [1, 2], [2, 3], [4, 4], [1, 5], [2, 6], [2, 7], [4, 8], [7, 9], [9, 10], [100, 20], [120, 80]];
+const arrLabels = [[2], [3], [5], [8], [6], [8], [9], [12], [16], [19], [120], [200]];
 
 // const arrTests = [[1, 1], [1, 2], [2, 3], [4, 4], [1, 5], [2, 6], [2, 7], [4, 8], [7, 9], [9, 10]];
 // const arrLabels = [[2], [3], [5], [8], [6], [8], [9], [12], [16], [19]];
@@ -39,8 +45,41 @@ const arrLabels = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
 // ]
 
 // Generate some synthetic data for training.
-const xs = tf.tensor1d(arrTests);
-const ys = tf.tensor1d(arrLabels);
+// const xs = tf.tensor1d(arrTests);
+// const ys = tf.tensor1d(arrLabels);
+
+// const xs = tf.tensor2d(arrTests, [12, 2]);
+// const ys = tf.tensor2d(arrLabels, [12, 1]);
+
+xs = tf.tensor2d(arrTests);
+ys = tf.tensor2d(arrLabels);
+// xs = tf.tensor2d([[0,0],[0,1],[1,0],[1,1]])
+// ys = tf.tensor2d([[0],[1],[1],[0]])
+
+var model = tf.sequential()
+model.add(tf.layers.dense({units:8, inputShape:2, activation: 'tanh'}))
+model.add(tf.layers.dense({units:1, }))
+model.compile({optimizer: tf.train.adam(), loss: tf.losses.meanSquaredError })
+// model.compile({optimizer: 'sgd', loss: 'binaryCrossentropy', lr:0.1})
+
+const fit = async () => {
+  await model.fit(xs, ys, {
+    batchSize: 2,
+    epochs: 1000
+  })
+  model.predict(xs).print()
+
+  await model.save('file://./saved-model');
+
+  const loadedModel = await tf.loadLayersModel('file://./saved-model/model.json');
+  console.log(`
+  
+  loaded model:
+  `)
+  loadedModel.predict(xs).print();
+}
+
+fit();
 
 // const xs = tf.tensor1d([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 // const ys = tf.tensor1d([1, 3, 5, 7, 9, 11, 13, 15, 17, 19]);
@@ -50,8 +89,8 @@ const ys = tf.tensor1d(arrLabels);
 
 
 // Train the model using the data.
-model.fit(xs, ys, {epochs: 10}).then(() => {
-  // Use the model to do inference on a data point the model hasn't seen before:
-  const prediction = model.predict(tf.tensor2d([120], [1, 1])).print();
-  // Open the browser devtools to see the output
-});
+// model.fit(xs, ys, {epochs: 10}).then(() => {
+//   // Use the model to do inference on a data point the model hasn't seen before:
+//   const prediction = model.predict(xs).print();
+//   // Open the browser devtools to see the output
+// });
